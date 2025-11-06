e7cb8d085775d498a957b25e319b2acc80443104
onyune <wjsdbwls0303@gmail.com>
2025-11-06 11:04:21 +0900

    basicSetting

---
+++ src/main/java/com/nhnacademy/gateway/config/ObjectMapperConfig.java
@@ -0,0 +1,11 @@
+package com.nhnacademy.gateway.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ObjectMapperConfig {
+    @Bean
+    public ObjectMapper objectMapper(){return new ObjectMapper();}
+}
